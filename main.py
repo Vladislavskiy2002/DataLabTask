8663db43796936ece351d06d7082230ed5f5e39d
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-09-07 12:55:59 +0300

    some changes

--- main.py
+++ main.py
@@ -125,11 +125,23 @@ INSERT INTO order_menu(
 #     cur.execute(sql.SQL("CREATE TABLE IF NOT EXISTS test_table (id int);"))
 
 global order
-
+products = []
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
 
+@app.get("/product/remove/{name}")
+async def root(name: str):
+    global products
+    select_value = (name,)
+    select_script = "SELECT * FROM menu where named = %s"
+    products.remove(name)
+    print(products)
+    return JSONResponse(status_code=status.HTTP_200_OK)
+@app.get("/product/all")
+async def root():
+    global products
+    return products
 @app.get("/orders")
 async def root():
     cur = con.cursor()
@@ -143,6 +155,7 @@ async def root():
 
 @app.get("/menus")
 async def root():
+    global products
     cur = con.cursor()
     cur.execute("SELECT * FROM menu")
     result = cur.fetchall()
@@ -151,67 +164,93 @@ async def root():
     con.commit()
     json_compatible_item_data = jsonable_encoder(result)
     return JSONResponse(content=json_compatible_item_data)
+@app.get("/menus/{message}")
+async def root(message:str):
+    global products
+    cur = con.cursor()
+    newmessege = message
+    select_value = (newmessege,)
+    select_script = "SELECT * FROM menu where named = %s"
+    cur.execute(select_script, select_value)
+    result = cur.fetchall()
+    if result is None:
+        return message + "isn't exist"
+    # con.commit()
+    json_compatible_item_data = jsonable_encoder(result)
+    products.append(message)
+    return JSONResponse(content=json_compatible_item_data)
 @app.post("/orders")
-def add_order(orderDTO: OrderDTO):
+def add_orderdto(order: OrderDTO):
     cur = con.cursor()
 
     # insert_menu_script = "Insert into menu(id,named,types,cost) VALUES (%s,%s,%s,%s)"
     insert_orders_script = "Insert into orders(created_date,updated_date,address) VALUES (%s,%s,%s)"
+    select_script = "SELECT MAX(id) FROM orders"
+    cur.execute(select_script)
     insert_menu_orders_script = "Insert into order_products(order_id, menu_id) VALUES (%s,%s)"
-
-
-    insert_orders_value = (orderDTO.id,date.today().isoformat(),date.today().isoformat(), orderDTO.address)
+    order.id = cur.fetchone()
+    insert_orders_value = (date.today().isoformat(),date.today().isoformat(), order.address)
     cur.execute(insert_orders_script, insert_orders_value)
     con.commit()
 
-
-    for product in orderDTO.products:
-        insert_menu_orders_value = (orderDTO.id, product.id)
+    for product in order.products:
+        insert_menu_orders_value = (order.id, product.id)
         cur.execute(insert_menu_orders_script, insert_menu_orders_value)
         con.commit()
 
     con.commit()
 
-@app.post("/orders")
-def add_order(message: str):
-    cur.execute("SELECT types from menu");
-    result = cur.fetchall()
-    if result is None:
-        return JSONResponse(content="orders haven't found", status_code=status.HTTP_404_NOT_FOUND)
-    products : List
-    while True :
-        for type in result:
-            if str == ("I'd like a " + type):
-                print("which do you prefer?")
-                # str = someMessage
-                cur.execute("SELECT named from menu");
-                result2 = cur.fetchall()
-                if result is None:
-                    return JSONResponse(content="product haven't found", status_code=status.HTTP_404_NOT_FOUND)
-                for name in result2:
-                    if (str == "I'd like a " + name):
-                        products.append(name)
-            elif str == ("I don't want a " + name):
-                products.remove(name)
-            elif str == "That's all":
-                print("Enter address: ")
-                address = "ANY ADDRESS"
-                order = OrderDTO
-                order.address = address
-                for product in products:
-                    select_script = cur.execute("SELECT * from menu where name = %s");
-                    select_value = (product,)
-                    cur.execute(select_script, select_value)
-                    order.products.append(cur.fetchone())
-                add_order(order)
-                break
-            elif str == ("Yes, please"):
-                name = "dsd"
-            elif str == ("No, thank you"):
-                name = "dsd"
-            else:
-                print("Sorry, i don't understand you!")
-                print("Try again :)")
+orderdto = OrderDTO
+@app.post("/orders/{address}")
+def add_order(address: str):
+    global orderdto
+    orderdto.address = address
+    if(products.__sizeof__() == 0):
+        for product in products:
+            print(product)
+            select_value = (product,)
+            select_script = "SELECT * FROM menu where named = %s"
+            cur.execute(select_script, select_value)
+            orderdto.products.append(Menu.parse_obj(cur.fetchone()))
+    add_orderdto(orderdto)
+
+    # cur.execute("SELECT types from menu");
+    # result = cur.fetchall()
+    # if result is None:
+    #     return JSONResponse(content="orders haven't found", status_code=status.HTTP_404_NOT_FOUND)
+    # products : List
+    # while True :
+    #     for type in result:
+    #         if str == ("I'd like a " + type):
+    #             print("which do you prefer?")
+    #             cur.execute("SELECT named from menu");
+    #             result2 = cur.fetchall()
+    #             if result is None:
+    #                 return JSONResponse(content="product haven't found", status_code=status.HTTP_404_NOT_FOUND)
+    #             for name in result2:
+    #                 if (str == "I'd like a " + name):
+    #                     products.append(name)
+    #         elif str == ("I don't want a " + name):
+    #             products.remove(name)
+    #         elif str == "That's all":
+    #             print("Enter address: ")
+    #             address = "ANY ADDRESS"
+    #             order = OrderDTO
+    #             order.address = address
+    #             for product in products:
+    #                 select_script = cur.execute("SELECT * from menu where name = %s");
+    #                 select_value = (product,)
+    #                 cur.execute(select_script, select_value)
+    #                 order.products.append(cur.fetchone())
+    #             add_order(order)
+    #             break
+    #         elif str == ("Yes, please"):
+    #             name = "dsd"
+    #         elif str == ("No, thank you"):
+    #             name = "dsd"
+    #         else:
+    #             print("Sorry, i don't understand you!")
+    #             print("Try again :)")
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
