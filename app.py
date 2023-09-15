ff5f7bc0dbfff07ed9e03c517528e5b271a44bfc
vladislavskiy <vladislavskiyproduction@gmail.com>
2023-09-15 04:43:51 +0300

    some changes

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ import time
 
 import requests
 
-st.title("Simple chat")
-
+st.title("COFFEE SHOt")
 
 # Initialize chat history
 if "messages" not in st.session_state:
@@ -18,55 +17,38 @@ for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
+flag = False
+# with st.chat_message("assistant"):
+#     dat = requests.get("http://127.0.0.1:8000/startprogram").json()
+#     if dat == "Welcome at the coffee shoP What would you like?":
+#         st.write(dat)
+#         st.session_state.messages.append({"role": "assistant", "content": dat})
 # Accept user input
 if prompt := st.chat_input("What is up?"):
     # Display user message in chat message container
     with st.chat_message("user"):
         st.markdown(prompt)
-    # Add user message to chat history
-    if prompt.startswith("I'd like a "):
-        prompt = prompt.replace("I'd like a ", '', 1) #I'd like a banana
-        print("prompt: " + prompt)
-        data = requests.get("http://127.0.0.1:8000/menus/" + prompt.__str__()).json()
-
-        ##st.chat_input("Would you like anything else?")
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        data = requests.get("http://127.0.0.1:8000/handler/" + prompt.__str__()).json()
         st.write(data)
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            message_placeholder.markdown("Would you like anything else?")
-        st.session_state.messages.append({"role": "assistant", "content": "Would you like anything else?"})
-        time.sleep(0.5)
-    if prompt.startswith("I don't want a "):
-        prompt = prompt.replace("I don't want a ", '', 1)  # I'd like a banana
-        print("prompt: " + prompt)
-        data = requests.get("http://127.0.0.1:8000/product/remove/" + prompt.__str__()).json()
-        st.write(data)
-    if prompt.startswith("That's all"):
-        with st.chat_message("assistant"):
-            message_placeholder = st.empty()
-            message_placeholder.markdown("YOUR ORDER IS: ")
-            data = requests.get("http://127.0.0.1:8000/product/all/" + prompt.__str__())
-            st.write(data)
-    st.session_state.messages.append({"role": "user", "content": prompt})
-
-# Display assistant response in chat message container
-with st.chat_message("assistant"):
-    message_placeholder = st.empty()
-    full_response = ""
-
-    assistant_response = random.choice(
-        [
-            "Hello there! How can I assist you today?",
-            "Hi, human! Is there anything I can help you with?",
-            "Do you need help?",
-        ]
-    )
-    # Simulate stream of response with milliseconds delay
-    for chunk in assistant_response.split():
-        full_response += chunk + " "
-        time.sleep(0.05)
-        # Add a blinking cursor to simulate typing
-        message_placeholder.markdown(full_response + "")
-    message_placeholder.markdown(full_response)
-# Add assistant response to chat history
-st.session_state.messages.append({"role": "assistant", "content": full_response})
+        st.session_state.messages.append({"role": "assistant", "content": data})
+    #     assistant_response = random.choice(
+    #         [
+    #                 "Hello there! How can I assist you today?",
+    #                 "Hi, human! Is there anything I can help you with?",
+    #                 "Do you need help?",
+    #         ]
+    #         )
+    #     # Simulate stream of response with milliseconds delay
+    #     for chunk in assistant_response.split():
+    #         full_response += chunk + " "
+    #         time.sleep(0.05)
+    #         # Add a blinking cursor to simulate typing
+    #         message_placeholder.markdown(full_response + "")
+    #     message_placeholder.markdown(full_response)
+    # # Add assistant response to chat history
+    # st.session_state.messages.append({"role": "assistant", "content": full_response})
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from starlette import status
 from starlette.responses import JSONResponse
+import requests
 
 app = FastAPI()
 
@@ -29,13 +30,14 @@ class Orderi(BaseModel):
     id:int
     created_date: date
     updated_date: date
-    address: str
 
 class OrderDTO(BaseModel):
     id: int
     address: str
-    products: List[Menu]
-
+    order_products: List[Menu]
+class OrderiDTO(BaseModel):
+    order_products: List[Menu]
+    total:str
 
 sql_schript1 = """  
 CREATE TABLE IF NOT EXISTS menu (
@@ -124,14 +126,87 @@ INSERT INTO order_menu(
 #     db_con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
 #     cur.execute(sql.SQL("CREATE TABLE IF NOT EXISTS test_table (id int);"))
 
-global order
 products = []
+flag = False
+start = False
+
+import openai
+
+openai.api_key = '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'
+
+#
+
+@app.get("/startprogram")
+def startProgramMessege():
+    global start
+    if start:
+        start = False
+        return "Welcome at the coffee shoP What would you like?"
+@app.get("/handler/{message}")
+async def root(message: str):
+    global flag
+    global start
+    if start:
+        start = False
+        return "Welcome at the coffee shop \n What would you like? "
+    if flag:
+        if message == "yes":
+            flag = False
+            products.append("deer")
+            return "The deer has been successfully added to your order"
+        elif message == "no":
+            flag = False
+            return "Would you like something else?"
+        else:
+            return "I don't understand you. Try again"
+
+    if message.startswith("I'd like a "):
+        prompt = message.replace("I'd like a ", '', 1)
+        print("start")
+        data = menusByName(prompt)
+        if data == prompt.__str__() + " isn't exist in our menu":
+            return prompt.__str__() + " isn't exist in our menu"
+        flag = True
+        return "Would you like a deer to your " + prompt.__str__() + "?"
+    elif message.startswith("I don't want a "):
+        prompt = message.replace("I don't want a ", '', 1)
+        if(products.__len__() ==0):
+            return "You can't do it because you haven't choose any product"
+        return removeProduct(prompt)
+    elif message.startswith("Show all"):
+        return allproducts()
+    elif message.startswith("That's all"):
+        total = totalcost()
+        add_orderdto(OrderiDTO)
+        return {"Total cost" : total}
+    elif message.startswith("What's"):
+        response = openai.Completion.create(
+            chat = "gpt-3.5-turbo",
+            engine="text-davinci-003",
+            prompt= message,
+            temperature=0.5,
+            max_tokens=100
+        )
+        return response.choices[0].text.strip()
+    else:
+        return "Sorry, but i don't understand you"
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
+def totalcost():
+    global products
+    total = 0
+    for product in products:
+        select_value = (product,)
+        print(product)
+        select_script = "SELECT cost FROM menu where named = %s"
+        cur.execute(select_script, select_value)
+        data = cur.fetchone()
+        total += data[0]
+    print("Totali" + total.__str__())
+    return total.__str__()
 @app.get("/product/remove/{name}")
-async def root(name: str):
+def removeProduct(name: str):
     global products
     select_value = (name,)
     select_script = "SELECT * FROM menu where named = %s"
@@ -139,8 +214,10 @@ async def root(name: str):
     print(products)
     return JSONResponse(status_code=status.HTTP_200_OK)
 @app.get("/product/all")
-async def root():
+def allproducts():
     global products
+    print(products)
+    json_compatible_item_data = jsonable_encoder(products)
     return products
 @app.get("/orders")
 async def root():
@@ -154,7 +231,7 @@ async def root():
     return JSONResponse(content=json_compatible_item_data)
 
 @app.get("/menus")
-async def root():
+def menus():
     global products
     cur = con.cursor()
     cur.execute("SELECT * FROM menu")
@@ -165,54 +242,64 @@ async def root():
     json_compatible_item_data = jsonable_encoder(result)
     return JSONResponse(content=json_compatible_item_data)
 @app.get("/menus/{message}")
-async def root(message:str):
+def menusByName(message:str):
     global products
-    cur = con.cursor()
-    newmessege = message
-    select_value = (newmessege,)
+    # print("fdfdff")
+    # return JSONResponse(status_code=status.HTTP_200_OK)    # cur = con.cursor()
+    select_value = (message,)
     select_script = "SELECT * FROM menu where named = %s"
     cur.execute(select_script, select_value)
     result = cur.fetchall()
-    if result is None:
-        return message + "isn't exist"
+    if result is None or result.__len__() == 0:
+        return message + " isn't exist in our menu"
     # con.commit()
     json_compatible_item_data = jsonable_encoder(result)
     products.append(message)
+    print(products)
     return JSONResponse(content=json_compatible_item_data)
 @app.post("/orders")
-def add_orderdto(order: OrderDTO):
+def add_orderdto(orders: OrderiDTO):
     cur = con.cursor()
 
     # insert_menu_script = "Insert into menu(id,named,types,cost) VALUES (%s,%s,%s,%s)"
-    insert_orders_script = "Insert into orders(created_date,updated_date,address) VALUES (%s,%s,%s)"
+    insert_orders_script = "Insert into orders(created_date,updated_date) VALUES (%s,%s)"
+
+    idProducts = []
+
+    for product in products:
+        print(product)
+        select_value = (product,)
+        select_script = "SELECT * FROM menu where named = %s"
+        cur.execute(select_script, select_value)
+
+        result = cur.fetchone()
+        # temp = Menu
+        idProducts.append(result[0])
+
+    products.clear()
+    insert_menu_orders_script = "Insert into order_products(order_id, menu_id) VALUES (%s,%s)"
+    insert_orders_value = (date.today().isoformat(),date.today().isoformat())
+    cur.execute(insert_orders_script, insert_orders_value)
+
     select_script = "SELECT MAX(id) FROM orders"
     cur.execute(select_script)
-    insert_menu_orders_script = "Insert into order_products(order_id, menu_id) VALUES (%s,%s)"
-    order.id = cur.fetchone()
-    insert_orders_value = (date.today().isoformat(),date.today().isoformat(), order.address)
-    cur.execute(insert_orders_script, insert_orders_value)
-    con.commit()
+    orders_result = cur.fetchone()
+    print(orders_result[0])
 
-    for product in order.products:
-        insert_menu_orders_value = (order.id, product.id)
+    print(idProducts)
+    con.commit()
+    for id in idProducts:
+        insert_menu_orders_value = (int.__int__(orders_result[0]), id)
         cur.execute(insert_menu_orders_script, insert_menu_orders_value)
         con.commit()
 
     con.commit()
 
-orderdto = OrderDTO
-@app.post("/orders/{address}")
-def add_order(address: str):
-    global orderdto
-    orderdto.address = address
-    if(products.__sizeof__() == 0):
-        for product in products:
-            print(product)
-            select_value = (product,)
-            select_script = "SELECT * FROM menu where named = %s"
-            cur.execute(select_script, select_value)
-            orderdto.products.append(Menu.parse_obj(cur.fetchone()))
-    add_orderdto(orderdto)
+
+# @app.post("/orders/{address}")
+# def add_order(address: str):
+#     OrderDTO.address = address
+#     add_orderdto(OrderDTO)
 
     # cur.execute("SELECT types from menu");
     # result = cur.fetchall()
